c386e821f0b23f64f3453e49d5c799767b7da7da
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2016-01-11 12:16:54 -0500

    Added help switch to binify and fixed help message

--- binify.ps1
+++ binify.ps1
@@ -1,7 +1,7 @@
 #*************************************************************
 #  File: binify.ps1
 #  Date created: 1/6/2016
-#  Date edited: 1/6/2016
+#  Date edited: 1/11/2016
 #  Author: Nathan Martindale
 #  Copyright © 2016 Digital Warrior Labs
 #  Description: Script that takes passed file and puts it into the temporary bin
@@ -12,15 +12,16 @@ param (
 	[string]$fileToBin = "",
 	
 	[switch]$list = $false,
-	[switch]$clear = $false
+	[switch]$clear = $false,
+	[switch]$help = $false
 )
 
 if ($fileToBin -ne "") { Copy-Item $fileToBin "/dwl/tmp/bin/" }
 if ($list) { Get-ChildItem "/dwl/tmp/bin/" }
 if ($clear) { Remove-Item "/dwl/tmp/bin/*" }
 
-if ($fileToBin -eq "" -and !$list -and !$clear)
+if ($fileToBin -eq "" -and !$list -and !$clear -or $help)
 {
 	echo "`nSends specified file to the temporary bin folder for quick testing/usage elsewhere, without having to go through the package system."
-	echo "`n`nUsage: `n`tbinify [path/name of script/runnable]`n`tbinify -list `t= displays all current contents of temporary bin`n`tbinify -clear `t= deletes all contents of temporary bin`n"
+	echo "`nUsage: `n`tbinify [path/name of script/runnable]`n`tbinify -list = displays all current contents of temporary bin`n`tbinify -clear = deletes all contents of temporary bin`n`tbinify -help = shows this help (DUH)`n"
 }
