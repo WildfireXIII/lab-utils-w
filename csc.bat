c880c03db64b5483d90cd7881192bc889106dfbe
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2016-02-27 10:29:50 -0600

    Added c# compiler batch

---
+++ csc.bat
@@ -0,0 +1,4 @@
+@echo off
+
+rem Thanks to http://stackoverflow.com/questions/553143/compiling-executing-a-c-sharp-source-file-in-command-prompt
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe %*
